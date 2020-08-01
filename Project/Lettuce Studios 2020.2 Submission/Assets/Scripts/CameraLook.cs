a79d07298c6eab2988dc4ab03bc0811f0c0a965f
LiquidZ <liquidzrunner@gmail.com>
2020-08-01 20:27:51 +0800

    basic WASD movement added

---
+++ Project/Lettuce Studios 2020.2 Submission/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public float sensitivity;
+    public GameObject player;
+    float mouseX;
+    float mouseY;
+    float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        Look();
+    }
+
+    void Look()
+    {
+        mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.transform.Rotate(Vector3.up * mouseX);
+
+    }
+
+}
