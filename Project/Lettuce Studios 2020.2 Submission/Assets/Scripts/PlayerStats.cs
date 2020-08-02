626719b03e701aab20eeba28f58dc518599d2726
LiquidZ <liquidzrunner@gmail.com>
2020-08-02 11:59:52 +0800

    implemented double jumping & player stats

---
+++ Project/Lettuce Studios 2020.2 Submission/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int health;
+
+    private void Update()
+    {
+        CheckIfDie();
+    }
+
+    void CheckIfDie()
+    {
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        //death ui
+        //idk what you want to add
+        Destroy(gameObject);
+    }
+
+}
