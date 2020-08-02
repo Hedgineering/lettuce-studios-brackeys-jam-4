94d27eb73010b94988436cc9fb6dfb822b73e5fc
kazexarts <68879376+kazexarts@users.noreply.github.com>
2020-08-02 04:37:54 +0200

    made some major changes

---
+++ Project/Lettuce Studios 2020.2 Submission/Assets/Scripts/Trial box spawn scripts/DiscSpinner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscSpinner : MonoBehaviour
+{
+
+    public float speed;
+
+    public bool Reverse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Reverse)
+        {
+
+            this.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,
+                transform.rotation.eulerAngles.y,
+                Time.deltaTime * -speed);
+        }
+        else
+        {
+
+            this.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,
+                transform.rotation.eulerAngles.y,
+                Time.deltaTime * speed);
+        }
+
+    }
+}
