8dd149a0c8d18e047f95786cb6d0f4a0ef3f4826
kazexarts <68879376+kazexarts@users.noreply.github.com>
2020-08-03 15:56:09 +0200

    update

--- Project/Lettuce Studios 2020.2 Submission/Assets/Scripts/Button.cs
+++ Project/Lettuce Studios 2020.2 Submission/Assets/Scripts/Button.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.EventSystems;
 
 public class Button : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class Button : MonoBehaviour
 
     public void click()
     {
+
         if (!clicked)
         {
             StartCoroutine(Click());
