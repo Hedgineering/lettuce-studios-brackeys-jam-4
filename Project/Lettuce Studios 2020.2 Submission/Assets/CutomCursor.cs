240dee802ce274965019a58ed573b16dc3dc36d0
conie151 <codingcween.0225@gmail.com>
2020-08-02 17:40:52 -0700

    Level Selection Menu
    I made the level selection menu (prototype) still has problems..(needs animations, and script for locked and click next level). Also, need to fix the cursor (it will not click atm, because I'ts a UI image i think)

---
+++ Project/Lettuce Studios 2020.2 Submission/Assets/CutomCursor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutomCursor : MonoBehaviour
+{
+    Vector2 targetPos;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = targetPos;
+    }
+}
