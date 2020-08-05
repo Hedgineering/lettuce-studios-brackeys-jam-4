f8708b74a14f12d560ff4b178649dd8629a7aec7
DESKTOP-C03N9QM\mewed <meweddie3@gmail.com>
2020-08-04 20:54:54 -0500

    Probvably

--- Project/Lettuce Studios 2020.2 Submission/Assets/Scripts/LevelSelect.cs
+++ Project/Lettuce Studios 2020.2 Submission/Assets/Scripts/LevelSelect.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class LevelSelect : MonoBehaviour
 {
     public Animator anim;
-
+    private void Start()
+    {
+        anim.speed = 0f;
+    }
     public void LevelsOpen()
     {
         anim.Play("LevelSwitchOpen", -1, 0f);
+        anim.speed = 1f;
     }
     public void LevelsClose()
     {
         anim.Play("LevelSwitchClose", -1, 0f);
+        anim.speed = 1f;
     }
 }
