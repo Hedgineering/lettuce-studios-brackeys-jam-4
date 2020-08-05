ea419b093cf507f5a25a548772c556972b15d6c9
DESKTOP-C03N9QM\mewed <meweddie3@gmail.com>
2020-08-05 17:30:21 -0500

    UI Update most bugs fixed

---
+++ Project/Lettuce Studios 2020.2 Submission/Assets/Scripts/CoroutineForUnlocked.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoroutineForUnlocked : MonoBehaviour
+{
+    public GameObject unlockScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    public void UnlockedScenOpen()
+    {
+        StartCoroutine(UnlockPause());
+    }
+    
+    public IEnumerator UnlockPause()
+    {
+        yield return new WaitForSeconds(2);
+        unlockScene.SetActive(true);
+    }
+}
