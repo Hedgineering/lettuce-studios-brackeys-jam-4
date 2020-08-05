5f6a956c0ada9e04858612f59011d5c83046996c
LiquidZ <liquidzrunner@gmail.com>
2020-08-05 19:55:58 +0800

    added pause menu (no GFX) at trial record player scene (press ESC)

---
+++ Project/Lettuce Studios 2020.2 Submission/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseUI;
+
+    private void Update()
+    {
+        Pause();
+    }
+
+    public void Pause()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseUI.SetActive(true);
+        }
+    }
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
