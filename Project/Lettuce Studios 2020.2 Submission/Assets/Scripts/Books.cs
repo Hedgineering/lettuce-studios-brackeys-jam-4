8c23ed700abce74af129b39c0241c247f3c1e8af
kazexarts <68879376+kazexarts@users.noreply.github.com>
2020-08-08 02:28:11 +0200

    push!

---
+++ Project/Lettuce Studios 2020.2 Submission/Assets/Scripts/Books.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Books : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+        StartCoroutine(Spawn());
+    }
+
+
+
+
+    public void damage()
+    {
+        StartCoroutine(Despawn());
+    }
+
+
+    public IEnumerator Despawn()
+    {
+        transform.DOShakeRotation(0.5f, 5, 5, 90, false);
+        yield return new WaitForSeconds(1);
+        transform.DOScale(0, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        gameObject.SetActive(false);
+    }
+
+    public IEnumerator Spawn()
+    {
+        transform.DOScale(1, 0.5f);
+        yield return new WaitForSeconds(0);
+        transform.DOShakeRotation(0.5f, 5, 5, 90, false);
+        
+    }
+}
--- Project/Lettuce Studios 2020.2 Submission/Assets/Scripts/Button.cs
+++ Project/Lettuce Studios 2020.2 Submission/Assets/Scripts/Button.cs
@@ -6,8 +6,10 @@ using UnityEngine.EventSystems;
 
 public class Button : MonoBehaviour
 {
-    bool clicked;
-    
+    private bool clicked;
+
+
+    [SerializeField] float buttonClickDepth;
 
     [SerializeField] float speed;
 
@@ -51,11 +53,16 @@ public class Button : MonoBehaviour
     IEnumerator Click()
     {
         clicked = true;
-        transform.DOMove(transform.position + Vector3.down * 0.02f, 0.3f, false);
+        transform.DOMove(transform.position + Vector3.down * buttonClickDepth, 0.3f, false);
         yield return new WaitForSeconds(0.5f);
-        transform.DOMove(transform.position + Vector3.up * 0.02f, 0.3f, false);
-        yield return new WaitForSeconds(0.5f);
+        transform.DOMove(transform.position + Vector3.up * buttonClickDepth, 0.3f, false);
+        yield return new WaitForSeconds(0.3f);
         clicked = false;
+    }
+
+    private void OnMouseDown()
+    {
+        click();
     }
 
     private void OnMouseDrag()
