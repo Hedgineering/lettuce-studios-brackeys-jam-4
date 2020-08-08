16cb3c1aa5df562a1c103750336a77155cfb25a8
Legolas1015 <rhegde.hrahul@gmail.com>
2020-08-07 20:33:43 -0400

    Added Music Functionality to game and updated MUSIC UI

---
+++ Project/Lettuce Studios 2020.2 Submission/Assets/Scripts/ChooseCustomSong.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+using NAudio;
+using NAudio.Wave;
+
+public class ChooseCustomSong : MonoBehaviour
+{
+    public string path { get; private set; }
+    private string outpath;
+    public TextMeshProUGUI pathText;
+    AudioSource song;
+    AudioClip musicfile;
+
+    private void Awake()
+    {
+        song = GetComponent<AudioSource>();
+    }
+
+    public void OpenExplorer()
+    {
+        path = EditorUtility.OpenFilePanel("Choose your song", "", ".mp3");
+        if (path != null)
+        {
+            pathText.SetText(path);
+            CreateOutPath(path);
+        }
+        StartCoroutine(SetSong());
+    }
+
+    private void CreateOutPath(string path)
+    {
+        string temp = path.Substring(0, path.LastIndexOf(".mp3"));
+        outpath = temp + ".wav";
+        print(outpath);
+    }
+
+    IEnumerator SetSong()
+    {
+        if (path != null)
+        {
+            using (UnityWebRequest unityWebRequest = UnityWebRequestMultimedia.GetAudioClip("file:///" + path, AudioType.MPEG))
+            {
+                yield return unityWebRequest.SendWebRequest();
+
+                if (unityWebRequest.isNetworkError)
+                {
+                    Debug.Log(unityWebRequest.error);
+                }
+                else
+                {
+                    //Converting MP3 to WAV because Unity won't play MP3 due to copyright issues
+                    //song.clip = DownloadHandlerAudioClip.GetContent(unityWebRequest);
+                    using (var reader = new Mp3FileReader(path))
+                    {
+                        WaveFileWriter.CreateWaveFile(outpath, reader);
+
+                        using (UnityWebRequest unityWebRequestWav = UnityWebRequestMultimedia.GetAudioClip("file:///" + outpath, AudioType.WAV))
+                        {
+                            yield return unityWebRequestWav.SendWebRequest();
+
+                            if (unityWebRequest.isNetworkError)
+                            {
+                                Debug.Log(unityWebRequestWav.error);
+                            }
+                            else
+                            {
+                                #region debugclip
+                                //song.clip = DownloadHandlerAudioClip.GetContent(unityWebRequestWav);
+                                //song.Play();
+                                //Playlist.Print();
+                                //song.clip = Playlist.getClip(10);
+                                //song.Play();
+                                //print("clip assigned!");
+                                #endregion debugclip
+
+                                Playlist.setClip(DownloadHandlerAudioClip.GetContent(unityWebRequestWav));
+                                
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Project/Lettuce Studios 2020.2 Submission/Assets/Scripts/Playlist.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playlist : MonoBehaviour
+{
+    private static AudioClip[] playlist = new AudioClip[11];
+
+    private void Awake()
+    {
+        playlist[0] = Resources.Load("Music/Tintamare_-_01_-_Ma_Tropicalit") as AudioClip;
+        playlist[1] = Resources.Load("Music/Tintamare_-_02_-_Caracas") as AudioClip;
+        playlist[2] = Resources.Load("Music/Tintamare_-_03_-_Knowlesville") as AudioClip;
+        playlist[3] = Resources.Load("Music/Tintamare_-_04_-_Propane") as AudioClip;
+        playlist[4] = Resources.Load("Music/Tintamare_-_05_-_Tannant") as AudioClip;
+        playlist[5] = Resources.Load("Music/Tintamare_-_06_-_La_Fin_des_Haricots") as AudioClip;
+        playlist[6] = Resources.Load("Music/Peyruis - Swing [Electro SwingDance] [CC BY]") as AudioClip;
+        playlist[7] = Resources.Load("Music/The Hikers - Le Swing [Electro Swing] [CC BY]") as AudioClip;
+        playlist[8] = Resources.Load("Music/The Musical Ghost - Potato Chips [Electro Swing] [CC BY]") as AudioClip;
+        playlist[9] = Resources.Load("Music/Amarià - Lovely Swindle [Electro Swing] [CC BY]") as AudioClip;
+        playlist[10] = Resources.Load("Music/EndMusic") as AudioClip;
+    }
+
+    //You may only change the last slot publicly
+    public static void setClip(AudioClip clip)
+    {
+        playlist[10] = clip;
+    }
+
+    public static AudioClip getClip(int i)
+    {
+        return playlist[i];
+    }
+
+    public static void ResetPlaylist()
+    {
+        playlist[10] = Resources.Load("EndMusic") as AudioClip;
+    }
+
+    public static void Print()
+    {
+        int index = 0;
+        foreach(AudioClip i in playlist)
+        {
+            print("Index " + index + ": " + i.name);
+            index++;
+        }
+    }
+}
